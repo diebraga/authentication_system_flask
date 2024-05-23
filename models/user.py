ae86eeb336df23f9144ee6a93326462a687f19c4
Diego Braga <diebraga0055@gmail.com>
2024-05-23 17:04:09 -0300

    pass

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     # classe qui définit l'utilisateur
     id: int = db.Column(db.Integer, primary_key=True)
     username: str = db.Column(db.String(80), unique=True, nullable=False)
-    username: str = db.Column(db.String(80), nullable=False)
+    password: str = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
